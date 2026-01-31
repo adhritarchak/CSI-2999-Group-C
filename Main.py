457a15ddce45ddb9f78f794a46948a4e7d370798
adhritarchak <adhritarchak@gmail.com>
2026-01-31 15:43:41 -0500

    Initial branch
    Added basic pygame shell on top

--- Main.py
+++ Main.py
@@ -1,7 +1,23 @@
 # Base file
+import pygame as pg
 
 def main():
-    pass
+    pg.init()
+    screen = pg.display.set_mode((800, 600))
+    pg.display.set_caption("My Game")
+    clock = pg.time.Clock()
+
+    running = True
+    while running:
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                running = False
+
+        screen.fill((0, 0, 0))  # Fill screen with black
+        pg.display.flip()
+        clock.tick(60)
+
+    pg.quit()
 
 if __name__ == "__main__":
     main()
