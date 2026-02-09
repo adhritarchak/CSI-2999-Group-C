e80b0323617b159820471be7b3a05283b8e20101
adhritarchak <adhritarchak@gmail.com>
2026-02-09 13:26:45 -0500

    Comments to explain the classes

--- Pong.py
+++ Pong.py
@@ -1,14 +1,19 @@
+# Base class for managing the game
 class PongGame:
     pass
 
+# Class to handle the players in the game
 class PongPlayer:
     pass
 
+# Class to manage the pong board
 class PongSpace:
     pass
 
+# Class to work the paddles
 class PongPaddle:
     pass
 
+# Class for the ball
 class PongBall:
     pass
