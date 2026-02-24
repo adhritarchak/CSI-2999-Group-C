02b6cec78943306d4454be0ba57461758a57753b
Adhrit Archak <adhritarchak@gmail.com>
2026-02-24 15:32:56 -0500

    Added a config file
    Added a config file so we don't have to specify all our variables at the top of the python file and take up a ton of space

--- Main.py
+++ Main.py
@@ -1,51 +1,73 @@
 # Base file
-import pygame as pg
-from Pong import *
-
-SCREEN_SIZE = (800, 600)
-
-class Paddle:
-    paddleSurface: pg.Surface
-    paddleImages: list[pg.Surface]
-
-    def __init__(self, assetList: list[str]):
-        paddleImages = list[pg.Surface]
-    def animate_paddle():
-        pass
-    def smash_hit(self, ball: Ball):
-        ball.impulse((ball.velocity()[X] * 1.5, ball.velocity()[Y] * 1.5, 0))
+import pygame
+import json
+# # Window Constants
 def main():
-    pg.init()
-    screen = pg.display.set_mode(SCREEN_SIZE)
-    pg.display.set_caption("My Game")
-    clock = pg.time.Clock()
-    if not pg.display.get_init():
-        pg.display.init()
-
-    # paddle = pg.image.load("assets/Red Paddle.png").convert_alpha()
-    # paddle_rect = paddle.get_rect(topleft=(100, 100))
-    # pg.draw.rect(screen, (255, 0, 0), paddle_rect)
-    
-    ball = Ball(x=30, y=300, height=100, speed_x=1.5, speed_y=1, radius=8)
-    ball.do_draw_prediction(True)
-    ball.set_bounds(top=100, bottom=SCREEN_SIZE[Y], left=0, right=SCREEN_SIZE[X])
-
-    running = True
-    while running:
-        for event in pg.event.get():
-            if event.type == pg.QUIT:
-                running = False
-            if event.type == pg.KEYDOWN:
-                if event.key == pg.K_SPACE:
-                    ball.do_draw_prediction(not ball.draw_prediction)
-
-        screen.fill((0, 0, 0))  # Fill screen with black
-        # screen.blit(paddle, paddle_rect)
-        ball.draw(screen)
-        pg.display.flip()
-        clock.tick(60)
-
-    pg.quit()
+    # config = {
+    #     'Window': {
+    #         'Width': 1000,
+    #         'Height': 600,
+    #         'FPS': 60,
+    #         'Caption': "Supreme Pong",
+    #         'font': ("timesnewroman", 36)
+    #     },
+    #     'Table Layout': {
+    #         'Table_Margin': 80,
+    #         'Table_Border': 5,
+    #         'Net_Thickness': 4,
+    #         'Midline_Thickness': 3
+    #     },
+    #     'Paddle Physics': {
+    #         'Paddle_Boost': 1.4,
+    #         'Paddle_Width': 20,
+    #         'Paddle_Height': 80,
+    #         'BasePaddleSpeed': 5,
+    #         'SmashPaddle1Speed': 1.5,       #BasePaddleSpeed * 0.3
+    #         'SmashPaddle2Speed': 1.5,       #BasePaddleSpeed * 0.3
+    #         'SmashChargeSpeed': 1.5,
+    #         'Smash_start_time': 0,
+    #         'Smash1_hold_time': 0,
+    #         'Smash1_hit': False,
+    #         'Smash1_hit_time': 0,
+    #         'Smash1_active': False,
+    #         'Smash2_hold_time': 0,
+    #         'Smash2_active': False,
+    #         'Smash_duration': 3000,
+    #         'Smash2_hit': False,
+    #         'Smash2_hit_time': 0
+    #     },
+    #     'Ball Physics': {
+    #         'Radius': 10,
+    #         'Max_Height': 25,
+    #         'Gravity': 0.35,
+    #         'BounceSpeedLost': 0.85,
+    #         'Max_Speed': 10,
+    #         'Bounce_MinZ': 8,
+    #         'paddle_hit_boost': 1,
+    #         'init_x': 500,      #Width // 2
+    #         'init_y': 300,      #Height // 2
+    #         'init_height': 0,
+    #         'init_vel_x': 1,
+    #         'init_vel_y': 0.8,
+    #         'init_vel_z': 0
+    #     },
+    #     'colors': {
+    #         'White': (255, 255, 255),
+    #         'Black': (0, 0, 0),
+    #         'Red Table': (170, 40, 40),
+    #         'Light Brown': (200, 170, 130),
+    #         'Orange': (255, 165, 0),
+    #         'Shadow': (0, 50, 0, 100),
+    #         'Paddle 1': (255, 100, 100),
+    #         'Paddle 2': (100, 100, 255)
+    #         }
+    # }
+    # with open('config.json', 'w') as c:
+    #     json.dump(config, c)
+    config: dict = None
+    with open('config.json', 'r') as c:
+        config = json.load(c)
+    print(config)
 
 if __name__ == "__main__":
     main()
--- game.py
+++ game.py
@@ -1,27 +1,26 @@
 import pygame
 import sys
-import cards 
-from ball import Ball
+from Pong import *
 pygame.init()
 
-# Window Constants
-Width = 1000
-Height = 600
-FPS = 60
-screen = pygame.display.set_mode((Width, Height))
-pygame.display.set_caption("Supreme Pong")
+with open('config.json') as f:
+    config = json.load(f)
+    screenConfig = config['Window']
+    tableConfig = config['Table Layout']
+    paddleConfig = config['Paddle Physics']
+    ballConfig = config['Ball Physics']
+    colorConfig = config['colors']
+
+# Window Setup
+screen = pygame.display.set_mode((screenConfig['Width'], screenConfig['Height']))
+pygame.display.set_caption(screenConfig['Caption'])
 clock = pygame.time.Clock()
-font = pygame.font.SysFont("timesnewroman", 36)
+font = pygame.font.SysFont(*screenConfig['font'])
 
-# Table Layout/Dimensions
-Table_Margin = 80
-Table_Border = 5
-Net_Thickness = 4
-Midline_Thickness = 3
-
-Table_Rect = pygame.Rect(Table_Margin, Table_Margin,
-                         Width - Table_Margin * 2,
-                         Height - Table_Margin * 2,)
+# Table Setup
+Table_Rect = pygame.Rect(tableConfig['Table_Margin'], tableConfig['Table_Margin'],
+                         screenConfig['Width'] - tableConfig['Table_Margin'] * 2,
+                         screenConfig['Height'] - tableConfig['Table_Margin'] * 2,)
 
 Left_Boundary = Table_Rect.left
 Right_Boundary = Table_Rect.right
@@ -31,14 +30,6 @@ Center_x = Table_Rect.centerx
 Center_y = Table_Rect.centery
 
 # Paddle Constants
-Paddle_Boost = 1.4
-Paddle_Width = 20
-Paddle_Height = 80
-BasePaddleSpeed = 5
-SmashPaddle1Speed = BasePaddleSpeed * 0.3
-SmashPaddle2Speed = BasePaddleSpeed * 0.3
-SmashChargeSpeed = 1.5
-Smash_start_time = 0
 Smash1_hold_time = 0
 Smash1_hit = False
 Smash1_hit_time = 0
@@ -49,54 +40,17 @@ Smash_duration = 3000
 Smash2_hit = False
 Smash2_hit_time = 0
 
-# Ball Constants
-Ball_Radius = 10
-Ball_Max_Height = 25
-
-# Ball Physics Constants
-ball_init_vel_x = 3
-ball_init_vel_y = 1.5
-ball_init_vel_z = 8
-Gravity = 0.35
-BounceSpeedLost = 0.85
-Max_Speed = 10
-Bounce_MinZ = 8
-paddle_hit_boost = 1
-
-#ball_characteristics = Ball ( ball_x = Width // 2, ball_y = Height // 2, ball_vel_x = 1, ball_vel_y = 0.8, ball_height = 0, ball_vel_z = 0, ball_max_height = 25)
-
-ball_x = Width // 2
-ball_y = Height // 2
-ball_vel_x = 1
-ball_vel_y = 0.8
-ball_height = 0
-ball_vel_z = 0
-
-
-
-
-# colors constants
-WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
-RED_TABLE = (170, 40, 40)
-LIGHT_BROWN = (200, 170, 130)
-ORANGE = (255, 165, 0)
-SHADOW = (0, 50, 0, 100)
-PADDLE1_COLOR = (255, 100, 100)
-PADDLE2_COLOR = (100, 100, 255)
-
-
 #sample paddles
 paddle1_x = Left_Boundary + 50
-paddle1_y = Center_y - Paddle_Height // 2
+paddle1_y = Center_y - paddleConfig['Paddle_Height'] // 2
 
-paddle2_x = Right_Boundary - 50 - Paddle_Width
-paddle2_y = Center_y - Paddle_Height // 2
+paddle2_x = Right_Boundary - 50 - paddleConfig['Paddle_Width']
+paddle2_y = Center_y - paddleConfig['Paddle_Height'] // 2
 
-paddle1 = PongPaddle(width=Paddle_Width, height=Paddle_Height, color = PADDLE1_COLOR, 
-                     initialPos=(paddle1_x, paddle1_y), speed=Paddle_Speed)
-paddle2 = PongPaddle(width=Paddle_Width, height=Paddle_Height, color=PADDLE2_COLOR, 
-                     initialPos=(paddle2_x, paddle2_y), speed=Paddle_Speed)
+paddle1 = PongPaddle(width=paddleConfig['Paddle_Width'], height=paddleConfig['Paddle_Height'], color = colorConfig['Paddle 1'], 
+                     initialPos=(paddle1_x, paddle1_y), speed=paddleConfig['BasePaddleSpeed'])
+paddle2 = PongPaddle(width=paddleConfig['Paddle_Width'], height=paddleConfig['Paddle_Height'], color=colorConfig['Paddle 2'], 
+                     initialPos=(paddle2_x, paddle2_y), speed=paddleConfig['BasePaddleSpeed'])
 
 paddle1.setBounds(Top_Boundary, Bot_Boundary, Left_Boundary, Right_Boundary)
 paddle2.setBounds(Top_Boundary, Bot_Boundary, Left_Boundary, Right_Boundary)
@@ -106,28 +60,28 @@ paddle2.setKeys(pygame.K_UP, pygame.K_DOWN, pygame.K_LEFT, pygame.K_RIGHT)
 
 def draw_table():
     # Background
-    screen.fill(LIGHT_BROWN)
-    shadow = Table_Rect.move(6, 6)
-    pygame.draw.rect(screen, BLACK, shadow, border_radius=6)
-    pygame.draw.rect(screen, RED_TABLE, Table_Rect)
+    screen.fill(colorConfig['Light Brown'])
+    colorConfig['Shadow'] = Table_Rect.move(6, 6)
+    pygame.draw.rect(screen, colorConfig['Black'], colorConfig['Shadow'], border_radius=6)
+    pygame.draw.rect(screen, colorConfig['Red Table'], Table_Rect)
 
     # Table border
-    pygame.draw.rect(screen, WHITE, Table_Rect, Table_Border)
+    pygame.draw.rect(screen, colorConfig['White'], Table_Rect, tableConfig['Table_Border'])
 
     # PingPong Net
     pygame.draw.line(
-        screen, WHITE,
+        screen, colorConfig['White'],
         (Table_Rect.centerx, Table_Rect.top),
         (Table_Rect.centerx, Table_Rect.bottom),
-        Net_Thickness
+        tableConfig['Net_Thickness']
     )
 
     # Center horizontal line
     pygame.draw.line(
-        screen, WHITE,
+        screen, colorConfig['White'],
         (Table_Rect.left, Table_Rect.centery),
         (Table_Rect.right, Table_Rect.centery),
-        Midline_Thickness
+        tableConfig['Midline_Thickness']
     )
 
 ball = Ball(x=100, y=300, height=50, speed_x=2, speed_y=1.2, radius=8)
@@ -139,25 +93,26 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+            continue
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_c:  # Press C to open cards
-                chosen_card = cards.draw_random_card(screen, font)
+                chosen_card = draw_random_card(screen, font)
                 if chosen_card:
-                    print("You picked:", chosen_card.name)
+                    print("You picked: ", chosen_card.name)
             if event.key == pygame.K_z and chosen_card is not None: # Press Z to activate the chosen card effect
                     ball_data = { ##place holder
-                    'x': ball_x,
-                    'y': ball_y,
-                    'vel_x': ball_vel_x,
-                    'vel_y': ball_vel_y,
-                    'vel_z': ball_vel_z,
-                    'height': ball_height,
-                    'radius': Ball_Radius
+                    'x': ballConfig['init_x'],
+                    'y': ballConfig['init_y'],
+                    'vel_x': ballConfig['init_vel_x'],
+                    'vel_y': ballConfig['init_vel_y'],
+                    'vel_z': ballConfig['init_vel_z'],
+                    'height': ballConfig['init_height'],
+                    'radius': ballConfig['Radius']
                          }
                     chosen_card.activate(ball_data) ##place holder for data, once we figure out which actual ball data we need I'll make it
                     print("Activated card effect:", chosen_card.name)
                     chosen_card = None  # Clear the chosen card after activation
-    dt = clock.tick(FPS)
+    dt = clock.tick(screenConfig['FPS'])
     keys = pygame.key.get_pressed()
 
     paddle1.process_keys(keys)
@@ -169,8 +124,8 @@ while running:
     if ball.height() < 15 and ball.within_rect(paddle2.get_rect(), paddle2.position) and ball.heightVelo() < 0:
         ball.bounce(-1, 0)
         ball.impulse((paddle2.velocity[X] * 0.5, paddle2.velocity[Y] * 0.5))
-    CurrentPaddle1Speed = SmashPaddle1Speed if Smash1_active else BasePaddleSpeed
-    CurrentPaddle2Speed = SmashPaddle2Speed if Smash2_active else BasePaddleSpeed       
+    CurrentPaddle1Speed = paddleConfig['SmashPaddle1Speed'] if Smash1_active else paddleConfig['BasePaddleSpeed']
+    CurrentPaddle2Speed = paddleConfig['SmashPaddle2Speed'] if Smash2_active else paddleConfig['BasePaddleSpeed']       
     
     # paddle 1 movement (WASD)
     if keys[pygame.K_w] and paddle1_y > Top_Boundary:
@@ -182,34 +137,34 @@ while running:
     if keys[pygame.K_d] and paddle1_x + 20 < Center_x:
         paddle1_x += CurrentPaddle1Speed * (dt / 16.67)
     if keys[pygame.K_SPACE]: # regular
-        if paddle1_x < ball_x < paddle1_x + 20 and paddle1_y < ball_y < paddle1_y + 80:
-            ball_vel_x += 3 
+        if paddle1_x < ballConfig['init_x'] < paddle1_x + 20 and paddle1_y < ballConfig['init_y'] < paddle1_y + 80:
+            ballConfig['init_vel_x'] += 3 
     if keys[pygame.K_LSHIFT]: #smash
         Smash1_hold_time += dt
-        CurrentPaddle1Speed = BasePaddleSpeed * 0.5
+        CurrentPaddle1Speed = paddleConfig['BasePaddleSpeed'] * 0.5
         if Smash1_hold_time >= 3000 and not Smash1_active:
             Smash1_active = True
             Smash1_start_time = pygame.time.get_ticks()
-            CurrentPaddle1Speed = SmashPaddle1Speed
+            CurrentPaddle1Speed = paddleConfig['SmashPaddle1Speed']
             Smash1_hit = False
     if keys[pygame.K_LSHIFT] == False and Smash1_active:
             if not Smash1_hit:
                 Smash1_active = False
                 Smash1_hold_time = 0
-                CurrentPaddle1Speed = BasePaddleSpeed
-    if Smash1_active == True and paddle1_x < ball_x < paddle1_x + 20 and paddle1_y < ball_y < paddle1_y + 80:
-        ball_vel_x = min(abs(ball_vel_x) + 5, Max_Speed)
+                CurrentPaddle1Speed = paddleConfig['BasePaddleSpeed']
+    if Smash1_active == True and paddle1_x < ballConfig['init_x'] < paddle1_x + 20 and paddle1_y < ballConfig['init_y'] < paddle1_y + 80:
+        ballConfig['init_vel_x'] = min(abs(ballConfig['init_vel_x']) + 5, ballConfig['Max_Speed'])
         Smash1_hit = True
-        CurrentPaddle1Speed = BasePaddleSpeed * 0.2
+        CurrentPaddle1Speed = paddleConfig['BasePaddleSpeed'] * 0.2
         Smash1_hold_time = 0
         Smash1_hit_time = pygame.time.get_ticks()
         
     if Smash1_hit:
         if pygame.time.get_ticks() - Smash1_hit_time >= 2000:  # 2 seconds recovery time
-            CurrentPaddle1Speed = BasePaddleSpeed
+            CurrentPaddle1Speed = paddleConfig['BasePaddleSpeed']
             Smash1_hit = False
 
-    #elif Smash1_active and not (paddle1_x < ball_x < paddle1_x + 20 and paddle1_y < ball_y < paddle1_y + 80):
+    #elif Smash1_active and not (paddle1_x < ballConfig['init_x'] < paddle1_x + 20 and paddle1_y < ballConfig['init_y'] < paddle1_y + 80):
        #Smash1_hit = False
 
     # paddle 2 movement (arrow keys)
@@ -222,78 +177,26 @@ while running:
     if keys[pygame.K_RIGHT] and paddle2_x + 20 < Right_Boundary:
         paddle2_x += CurrentPaddle2Speed * (dt / 16.67)
     if keys[pygame.K_RETURN]: # regular
-        if paddle2_x < ball_x < paddle2_x + 20 and paddle2_y < ball_y < paddle2_y + 80:
-            ball_vel_x -= 3 
+        if paddle2_x < ballConfig['init_x'] < paddle2_x + 20 and paddle2_y < ballConfig['init_y'] < paddle2_y + 80:
+            ballConfig['init_vel_x'] -= 3 
     if keys[pygame.K_RSHIFT]: #smash
         Smash2_hold_time += dt
     if Smash2_hold_time >= 3000 and not Smash2_active:
             Smash2_active = True
             Smash2_start_time = pygame.time.get_ticks()
-            CurrentPaddle2Speed = SmashPaddle2Speed
+            CurrentPaddle2Speed = paddleConfig['SmashPaddle2Speed']
             Smash2_hit = False
     if Smash2_active == True:
-            if paddle2_x < ball_x < paddle2_x + 20 and paddle2_y < ball_y < paddle2_y + 80:
-                ball_vel_x += 5
+            if paddle2_x < ballConfig['init_x'] < paddle2_x + 20 and paddle2_y < ballConfig['init_y'] < paddle2_y + 80:
+                ballConfig['init_vel_x'] += 5
                 Smash2_hit = True
     if Smash2_active and pygame.time.get_ticks() - Smash2_start_time >= Smash_duration:
             Smash2_active = False
             Smash2_hold_time = 0
-            CurrentPaddle2Speed = BasePaddleSpeed 
+            CurrentPaddle2Speed = paddleConfig['BasePaddleSpeed'] 
             Smash2_hit = False
 
-    # Trying to make a ball
-    ball_vel_z -= Gravity
-    ball_height += ball_vel_z
-
-    # bounce when ball hits table
-    if ball_height <= 0:
-        ball_height = 0
-        ball_vel_z = abs(ball_vel_z) * BounceSpeedLost
-
-        # reduce speed on bounce
-        ball_vel_x *= 0.9
-        ball_vel_y *= 0.9
-
-        # keeping ball velocity
-        if ball_vel_z < Bounce_MinZ:
-            ball_vel_z = Bounce_MinZ
-
-    ball_x += ball_vel_x
-    ball_y += ball_vel_y
-
-    # ball collision with walls
-    if ball_x - Ball_Radius <= Left_Boundary or ball_x + Ball_Radius >= Right_Boundary:
-        ball_vel_x *= -1
-        ball_x = max(Left_Boundary + Ball_Radius, min(ball_x, Right_Boundary - Ball_Radius))
-
-    if ball_y - Ball_Radius <= Top_Boundary or ball_y + Ball_Radius >= Bot_Boundary:
-        ball_vel_y *= -1
-        ball_y = max(Top_Boundary + Ball_Radius, min(ball_y, Bot_Boundary - Ball_Radius))
-
-    #ball collision with paddles
-    if ball_height < 15:
-        p1_hit = (paddle1_x < ball_x < paddle1_x + Paddle_Width and 
-                  paddle1_y < ball_y < paddle1_y + Paddle_Height)
-        p2_hit = (paddle2_x < ball_x < paddle2_x + Paddle_Width and 
-                  paddle2_y < ball_y < paddle2_y + Paddle_Height)
-
-        if p1_hit and ball_vel_x < 0:
-
-            ball_vel_x = min(abs(ball_vel_x) + paddle_hit_boost, Max_Speed)
-            ball_vel_y = (min(abs(ball_vel_y), Max_Speed) * (1 if ball_vel_y > 0 else -1))
-            ball_vel_z = Bounce_MinZ
-
-        if p2_hit and ball_vel_x > 0:
-            ball_vel_x = max(-(abs(ball_vel_x) + paddle_hit_boost), -Max_Speed)
-            ball_vel_y = (min(abs(ball_vel_y), Max_Speed) * (1 if ball_vel_y > 0 else -1))
-            ball_vel_z = Bounce_MinZ
-
-    
-
     draw_table()
-
-    screen.blit(paddle1_surface, (paddle1_x, paddle1_y))
-    screen.blit(paddle2_surface, (paddle2_x, paddle2_y))
 
     ball.draw(screen=screen)
     pygame.display.flip()
